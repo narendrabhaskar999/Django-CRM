fcfb6e722711be275f769f05c066eee03019328a
Narendran Bhaskaran <narendrabhaskar999@gmail.com>
2025-03-21 10:20:27 +0100

    registration

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'dcrm.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,3 +127,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
